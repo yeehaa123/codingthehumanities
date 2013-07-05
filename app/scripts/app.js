3b7e9ef6461e89d818890b15417c8052721872f9
Yeehaa <medialoperations@gmail.com>
2013-07-05 12:40:16 -0700

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('codingTheHumanitiesApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
